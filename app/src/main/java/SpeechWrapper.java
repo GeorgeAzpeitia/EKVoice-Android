0ca855cf217faadde55c1ac854537a9f819744e8
Lou George <loug38@gmail.com>
2016-01-30 19:08:15 -0800

    Started adding online speech Recognition.

--- app/src/main/java/SpeechWrapper.java
+++ app/src/main/java/SpeechWrapper.java
@@ -1,5 +1,33 @@
 /**
  * Created by george on 1/30/16.
  */
+
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.java.util.ArrayList;
+
+import java.lang.Override;
+
+//I'm guessing this is where we'll have online recognition -Lou
 public class SpeechWrapper {
+    private String note = "";
+    private Button recordButton;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode){
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && data != null){
+                    ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    returnedText.setText(results.get(0));
+                }
+                break;
+            }
+        }
+
+    }
+
 }
