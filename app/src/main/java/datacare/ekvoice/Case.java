ac543b11c723a186e20afb95d7fad82843454ad2
Robin Singh <irobinsingh@yahoo.com>
2016-03-05 15:10:41 -0800

    Added a bunch of files

---
+++ app/src/main/java/datacare/ekvoice/Case.java
@@ -0,0 +1,17 @@
+package datacare.ekvoice;
+
+/**
+ * Created by Robin on 3/5/2016.
+ */
+public class Case {
+    public String lastName;
+    public String firstName;
+    public String claimNumber;
+    public String address;
+    public int phoneNumber;
+    public Contact manager;
+    public Contact carrier;
+    public Contact employer;
+    public Contact attorney;
+    public Contact serviceProvider;
+}
---
+++ app/src/main/java/datacare/ekvoice/Contact.java
@@ -0,0 +1,20 @@
+package datacare.ekvoice;
+
+/**
+ * Created by Robin on 3/5/2016.
+ */
+public class Contact {
+    public String name;
+    public String phoneNumber;
+    public String fax;
+    public String company;
+    public String address;
+    public String city;
+    public String state;
+    public String email;
+    public boolean sendEmailOk;
+    public boolean sendFaxOk;
+    public boolean sendMailOk;
+    public String history;
+    public String notes;
+}
