c8fb3a9c770bc4196cef2257c1f2d8a44763f9de
Robin Singh <irobinsingh@yahoo.com>
2016-03-06 12:38:07 -0800

    classes added

---
+++ app/src/main/java/datacare/ekvoice/CaseList.java
@@ -0,0 +1,38 @@
+package datacare.ekvoice;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class CaseList extends Activity {
+    //private CustomAdapter ca;
+    //private CaseAdapter ia;
+    private ListView listView;
+    private ArrayList<Case> caseNames = new ArrayList<>();
+    ArrayAdapter<Case> itemsAdapter =
+            new ArrayAdapter<Case>(this, android.R.layout.simple_list_item_1, caseNames);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_case_list);
+        Case test = new Case();
+        test.firstName = "Bernard";
+        test.lastName = "Sanders";
+        Contact company = new Contact();
+        company.name = "Independent";
+        test.employer = company;
+        caseNames.add(test);
+
+        //ca = new CustomAdapter(this);
+        //ia = new CaseAdapter(this);
+
+        listView = (ListView) findViewById(R.id.listV);
+        //listView.setAdapter(ia);
+        //ia.loadObjects();
+    }
+
+}
