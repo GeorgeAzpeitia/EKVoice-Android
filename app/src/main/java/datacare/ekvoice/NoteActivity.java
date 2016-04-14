11fac3692bb9bfccfe12d010e34b0d2e9e546072
Octavio <Octavio@dhcp-169-233-167-245.cruznet.ucsc.edu>
2016-04-14 13:29:28 -0700

    added the note activity and xml file

---
+++ app/src/main/java/datacare/ekvoice/NoteActivity.java
@@ -0,0 +1,13 @@
+package datacare.ekvoice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NoteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+    }
+}
