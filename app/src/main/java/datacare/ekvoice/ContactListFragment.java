e4ed41755c34dc39905a201d08d63e646047ef7e
Octavio <Octavio@Octavios-MacBook-Pro.local>
2016-04-14 18:22:20 -0700

    added more buttons to the note layout and tried to link and intent from the ContactListFragment to the note activity

--- app/src/main/java/datacare/ekvoice/ContactListFragment.java
+++ app/src/main/java/datacare/ekvoice/ContactListFragment.java
@@ -1,11 +1,14 @@
 package datacare.ekvoice;
 
 import android.app.ListFragment;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by george on 4/11/16.
@@ -25,4 +28,12 @@ public class ContactListFragment extends Fragment {
         View v = inflater.inflate(R.layout.contacts_list, container, false);
         return v;
     }
+
+    public void addNote(View view){
+        Intent note;
+        note = new Intent(getActivity() ,NoteActivity.class);
+        startActivity(note);
+
+    }
+
 }
