e0c13d0a23a3ced9ff38ac9f0a2132f65a86418e
George Azpeitia <gazpeiti@ucsc.edu>
2016-04-19 12:25:00 -0700

    Added add note button functionality

--- app/src/main/java/datacare/ekvoice/ContactListFragment.java
+++ app/src/main/java/datacare/ekvoice/ContactListFragment.java
@@ -2,12 +2,15 @@ package datacare.ekvoice;
 
 import android.app.ListFragment;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,15 +31,28 @@ public class ContactListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle state) {
         View v = inflater.inflate(R.layout.contacts_list, container, false);
+        ContactListAdapter contactsAdapter = new ContactListAdapter(getActivity(), contacts);
+        ListView cList = (ListView) v.findViewById(R.id.contactsList);
+        cList.setAdapter(contactsAdapter);
+
+        Button addNote = (Button) v.findViewById(R.id.button9);
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent note = new Intent(getActivity(), NoteActivity.class);
+                startActivity(note);
+            }
+        });
         return v;
     }
     public static Fragment newInstance(ArrayList<Contact> contactsParam){
         ContactListFragment frag = new ContactListFragment();
-        if (contactsParam != null) {
+        if (contactsParam != null && contactsParam.size() != 0) {
             frag.contacts = contactsParam;
         }
         return frag;
     }
+
     private static class ContactListAdapter extends ArrayAdapter<Contact> {
         public ContactListAdapter(Context context, ArrayList<Contact> users) {
             super(context, 0, users);
